ac53828608a6355d16c19ed38c908c29cc98d343
Vinh Vu <vinhhv@uci.edu>
2024-02-13 16:49:03 -0800

    Add multiple windows for desktop

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
+    // trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.multiplatformPlugin).apply(false)
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,24 +1,72 @@
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyShortcut
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.misterjvm.compose.ui.MainView
 import com.misterjvm.compose.ui.MyApplicationTheme
 
+data class WindowInfo(val windowName: String, val windowState: WindowState)
+
 fun main() {
     application {
-        val windowState = rememberWindowState()
+        var initialized by remember { mutableStateOf(false) }
+        var windowCount by remember { mutableStateOf(1) }
+        val windowList = remember { SnapshotStateList<WindowInfo>() }
+        // Add initial window
+        if (!initialized) {
+            windowList.add(WindowInfo("Timezone-$windowCount", rememberWindowState()))
+            initialized = true
+        }
 
-        Window(
-            onCloseRequest = ::exitApplication,
-            state = windowState,
-            title = "TimeZone",
-        ) {
-            Surface(modifier = Modifier.fillMaxSize()) {
-                MyApplicationTheme {
-                    MainView()
+        windowList.forEachIndexed { i, _ ->
+            Window(
+                onCloseRequest = { windowList.removeAt(i) },
+                state = windowList[i].windowState,
+                title = windowList[i].windowName,
+            ) {
+                MenuBar {
+                    Menu("File", mnemonic = 'F') {
+                        val nextWindowState = rememberWindowState()
+                        Item(
+                            "New",
+                            onClick = {
+                                windowCount++
+                                windowList.add(
+                                    WindowInfo(
+                                        "Timezone-$windowCount",
+                                        nextWindowState,
+                                    ),
+                                )
+                            },
+                            shortcut = KeyShortcut(Key.N, ctrl = true),
+                        )
+                        Item("Open", onClick = {}, shortcut = KeyShortcut(Key.O, ctrl = true))
+                        Item("Close", onClick = { windowList.removeAt(i) }, shortcut = KeyShortcut(Key.W, ctrl = true))
+                        Item("Save", onClick = {}, shortcut = KeyShortcut(Key.S, ctrl = true))
+                        Separator()
+                        Item("Exit", onClick = { windowList.clear() })
+                    }
+                    Menu("Edit", mnemonic = 'E') {
+                        Item("Cut", onClick = {}, shortcut = KeyShortcut(Key.X, ctrl = true))
+                        Item("Copy", onClick = {}, shortcut = KeyShortcut(Key.C, ctrl = true))
+                        Item("Paste", onClick = {}, shortcut = KeyShortcut(Key.V, ctrl = true))
+                    }
+                }
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MyApplicationTheme {
+                        MainView()
+                    }
                 }
             }
         }
